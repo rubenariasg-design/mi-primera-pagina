dd905623be69039743f36341c3a994107c0e680e
Ruben Arias gomez <ruben.ariasg@est.iudigital.edu.co>
2025-11-05 23:03:30 -0500

    Create mi.primer.repo.py

---
+++ mi.primer.repo.py
@@ -0,0 +1,21 @@
+#unidad 1
+""""
+¿Qué es un programa?
+Un programa es un conjunto de instrucciones que le indican a una computadora cómo realizar una tarea específica. Es como una receta que la computadora sigue para lograr un objetivo.
+Los programas están escritos en lenguajes de programación, que son lenguajes formales diseñados para ser entendidos tanto por humanos como por máquinas.
+Variables:
+Una variable es un nombre que se le da a una ubicación en la memoria de la computadora donde se almacena un valor. Es como una etiqueta que permite acceder y modificar ese valor.
+Las variables tienen un tipo de dato asociado (por ejemplo, entero, decimal, texto) que indica qué tipo de valor pueden almacenar.
+  
+Usamos IDEs como google colab para escribir y probar codigo en la nube sin instalaciones.
+
+Antes de programar,diseña el algoritmo paso a paso con instruciones claras y una condicion de finalizacion.
+
+Todo algoritmo tiene tres partes:Entrada de datos,procesamiento y salida.
+
+En el codigo usamos variables (valores que cambian) y constantes (valores fijos ) con tipos de datos especificos (entero,real,texto,booleano)que determina las operaciones validas
+"""
+A=34
+B=12
+C = A + B
+Print(c)
